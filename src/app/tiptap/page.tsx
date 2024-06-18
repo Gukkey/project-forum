c4647960c5078dd9a2f2f8694c7386868911c923
Gukkey <gukkey@protonmail.com>
2024-06-18 12:53:59 +0530

    fix(tiptap): fixed tiptap import
    fixed tiptap import which threw error when vercel tried to deploy

--- src/app/tiptap/page.tsx
+++ src/app/tiptap/page.tsx
@@ -1,4 +1,4 @@
-import Tiptap from "@projectforum/components/TipTap"
+import Tiptap from "@projectforum/components/Tiptap"
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">

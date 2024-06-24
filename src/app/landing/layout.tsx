d6b1e06c2a81672d4ada5bd1aa3d2f3f9e167c7f
shiyaam <63956643+shiyaamsunder@users.noreply.github.com>
2024-06-24 23:08:19 +0530

    style(landing): add consistent styling

--- src/app/landing/layout.tsx
+++ src/app/landing/layout.tsx
@@ -3,12 +3,13 @@ export default function LandingPageLayout({ children }: { children: React.ReactN
     <section
       style={{
         background: `
-            linear-gradient(90deg, #F0F7F4 calc(22px - 2px), transparent 1%) center / 22px 22px,
-            linear-gradient(#F0F7F4 calc(22px - 2px), transparent 1%) center / 22px 22px,
-            #D2D6DB
-          `
+          linear-gradient(90deg, #030711 calc(22px - 2px), transparent 100%) center / 22px 22px,
+          linear-gradient(#030711 calc(22px - 2px), transparent 100%) center / 22px 22px,
+          gray
+        `
       }}
-      className="min-h-screen">
+      className="min-h-screen  "
+    >
       {children}
     </section>
   )
--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,16 +1,17 @@
+import { Button } from "@shadcn/button"
+import Link from "next/link"
+
 export default function LandingPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="font-montserrat font-bold text-black text-6xl text-center mb-6">
-        Project Forum
-      </h1>
+      <h1 className=" font-bold text-6xl text-center mb-6">Project Forum</h1>
       <div className="flex space-x-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-          Sign In
-        </button>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-          Sign Up
-        </button>
+        <Link href="/signin">
+          <Button>Sign In</Button>
+        </Link>
+        <Link href="/signup">
+          <Button>Sign Up</Button>
+        </Link>
       </div>
     </div>
   )

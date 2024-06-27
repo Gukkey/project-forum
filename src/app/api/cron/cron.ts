dcd8d528356006d853b89c5b10a6f9c511a15ec6
Gukkey <gukkey@protonmail.com>
2024-06-27 22:12:50 +0530

    chore(merge): merge main to current branch

--- src/app/api/cron/cron.ts
+++ src/app/api/cron/cron.ts
@@ -1,12 +1,6 @@
 import { Redis } from "@upstash/redis"
 import { CronJob } from "cron"
-import { addAnimeInRedis, logCronJob } from "./helper"
-import { fetchAnime } from "./anilist"
-import { createDiscussionThread } from "@projectforum/server/db/queries"
-import { discussionThreads } from "@projectforum/server/db/schema"
-import { db } from "@projectforum/server/db"
-import { title } from "process"
-import { eq } from "drizzle-orm"
+import { addAnimeInRedis, checkIfAnimeReleasesSoon, logCronJob } from "./helper"
 
 export const fetchCache = "force-no-store"
 
@@ -28,61 +22,8 @@ export const job = new CronJob("*/10 * * * *", async () => {
       }
     })
 
-    const currentTime = Math.floor(new Date().getTime() / 1000.0) // Converting seconds to milliseconds
-    // await Promise.all(
     anime.map(async (animeId, idx) => {
-      const value = await redis
-        .get(animeId.toString(10))
-        .catch((err) => `${err instanceof Error ? err.stack : err}`)
-      if (value === null) {
-        logCronJob(`${animeId} returned NaN value ${value} index: ${idx}`)
-      }
-
-      const timestamp = Number(value)
-      const data = await fetchAnime(animeId)
-
-      if (timestamp > currentTime && timestamp - currentTime <= 10 * 60) {
-        console.log(`${timestamp} ${currentTime}`)
-        logCronJob(`timestamp: ${timestamp} currentTime: ${currentTime} idx: ${idx}`)
-        try {
-          const animeName = data.data.Media.title.english
-          const episode = data.data.Media.nextAiringEpisode.episode
-          const post = {
-            title: `${animeName} - EPISODE ${episode} DISCUSSION`,
-            content: `This thread is automated`,
-            sectionId: process.env.CRON_JOB_SECTION_ID as string,
-            topicId: process.env.CRON_JOB_TOPIC_ID as string,
-            userId: process.env.CRON_JOB_TOPIC_ID as string
-          }
-
-          const nextEpisode = episode + 1
-
-          const checkForTitle = await db
-            .select()
-            .from(discussionThreads)
-            .where(eq(discussionThreads.title, title))
-
-          if (checkForTitle.length > 0) {
-            logCronJob(`duplicate post ${title} index: ${idx}`)
-          }
-
-          createDiscussionThread(post).then(async () => {
-            const nextEpisodeData = await fetchAnime(nextEpisode)
-            await redis
-              .set(animeId.toString(10), data.data.Media.nextAiringEpisode.airingAt)
-              .catch((err) => {
-                logCronJob(
-                  `${err instanceof Error ? err.stack : err} redis key value: ${animeId.toString(10)} ${nextEpisodeData.data.Media.nextAiringEpisode.airingAt} index: ${idx}`
-                )
-              })
-          })
-          logCronJob(`${timestamp - currentTime} idx: ${idx}`)
-          logCronJob(`A new episode thread for ${post.title} has been released... index: ${idx}`)
-        } catch (err) {
-          logCronJob(`${err instanceof Error ? err.stack : err} index: ${idx}`)
-        }
-        console.log(`${timestamp} ${currentTime}`)
-      }
+      checkIfAnimeReleasesSoon(animeId, idx)
     })
     logCronJob(`Cron job completed, no anime is added.`)
   } catch (err) {
--- src/app/api/cron/helper.ts
+++ src/app/api/cron/helper.ts
@@ -1,7 +1,10 @@
 import { Redis } from "@upstash/redis"
 import { fetchAnime } from "./anilist"
-import { createCronJobLogs } from "@projectforum/server/db/queries"
+import { createCronJobLogs, createDiscussionThread } from "@projectforum/server/db/queries"
 import { NextResponse } from "next/server"
+import { discussionThreads } from "@projectforum/server/db/schema"
+import { db } from "@projectforum/server/db"
+import { eq } from "drizzle-orm"
 
 const redis = new Redis({
   url: process.env.REDIS_URL,
@@ -30,4 +33,70 @@ export function logCronJob(
     ...(nothingHappened !== undefined && { nothingHappened })
   }
   createCronJobLogs(log)
+}
+
+export async function addAnimeInDiscussionThread(animeId: number, idx: number) {
+  const data = await fetchAnime(animeId)
+  try {
+    const animeName = data.data.Media.title.english
+    const episode = data.data.Media.nextAiringEpisode.episode
+    const post = {
+      title: `${animeName} - EPISODE ${episode} DISCUSSION`,
+      content: `This thread is automated`,
+      sectionId: process.env.CRON_JOB_SECTION_ID as string,
+      topicId: process.env.CRON_JOB_TOPIC_ID as string,
+      userId: process.env.CRON_JOB_TOPIC_ID as string
+    }
+
+    const nextEpisode = episode + 1
+
+    const checkForTitle = await db
+      .select()
+      .from(discussionThreads)
+      .where(eq(discussionThreads.title, post.title))
+
+    if (checkForTitle.length > 0) {
+      logCronJob(`duplicate post ${post.title} index: ${idx}`)
+    }
+
+    createDiscussionThread(post).then(async () => {
+      const nextEpisodeData = await fetchAnime(nextEpisode)
+      await redis
+        .set(animeId.toString(10), data.data.Media.nextAiringEpisode.airingAt)
+        .catch((err) => {
+          logCronJob(
+            `${err instanceof Error ? err.stack : err} redis key value: ${animeId.toString(10)} ${nextEpisodeData.data.Media.nextAiringEpisode.airingAt} index: ${idx}`
+          )
+        })
+    })
+    logCronJob(`A new episode thread for ${post.title} has been released... index: ${idx}`)
+  } catch (err) {
+    logCronJob(`${err instanceof Error ? err.stack : err} index: ${idx}`)
+  }
+}
+
+export async function checkIfAnimeReleasesSoon(animeId: number, idx: number) {
+  // gets the unix timestamp value (in seconds) for the upcoming episode
+  const value = await redis
+    .get(animeId.toString(10))
+    .catch((err) => `${err instanceof Error ? err.stack : err}`)
+
+  if (value === null) {
+    logCronJob(`${animeId} returned NaN value ${value} index: ${idx}`)
+  }
+
+  const timestamp = Number(value)
+  const currentTime = Math.floor(new Date().getTime() / 1000.0) // Converting seconds to milliseconds
+
+  // checks if the current time is in 10 minutes window after the anime episode gets released
+  if (timestamp >= currentTime && timestamp - currentTime <= 10 * 60) {
+    logCronJob(`timestamp: ${timestamp} currentTime: ${currentTime} anime id: ${animeId}`)
+
+    // invokes addAnimeInDiscussuinThread and then checks again whether the next episode just got released
+    // now two episodes getting released in same time is rare but we are covering this
+    addAnimeInDiscussionThread(animeId, idx)
+    checkIfAnimeReleasesSoon(animeId, idx)
+  } else {
+    return
+  }
 }

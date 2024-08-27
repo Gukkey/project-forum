4161902b56c310b8a0574778e22ed96e038e23c7
shiyaam <63956643+shiyaamsunder@users.noreply.github.com>
2024-08-27 23:33:18 +0530

    chore(env): remove unused drizzle types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,34 +1,3 @@
-import {
-  cronJobLogs,
-  discussionThreads,
-  replies,
-  roles,
-  sections,
-  topics,
-  users
-} from "@projectforum/server/db/schema"
-
-export type InsertSection = typeof sections.$inferInsert
-export type SelectSection = typeof sections.$inferSelect
-
-export type InsertTopic = typeof topics.$inferInsert
-export type SelectTopic = typeof topics.$inferSelect
-
-export type InsertDiscussionThreads = typeof discussionThreads.$inferInsert
-export type SelectDiscussionThreads = typeof discussionThreads.$inferSelect
-
-export type InsertCronJobLogs = typeof cronJobLogs.$inferInsert
-export type SelectCronJobLogs = typeof cronJobLogs.$inferSelect
-
-export type InsertUser = typeof users.$inferInsert
-export type SelectUser = typeof users.$inferSelect
-
-export type InsertReplies = typeof replies.$inferInsert
-export type SelectReplies = typeof replies.$inferSelect
-
-export type InsertRole = typeof roles.$inferInsert
-export type SelectRole = typeof roles.$inferSelect
-
 export type TipTapInputProps = {
   text: string
 }

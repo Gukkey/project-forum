770af52988d16075ff0cb82b02e3d4a8ef24e1db
shiyaam <63956643+shiyaamsunder@users.noreply.github.com>
2024-09-16 22:28:28 +0530

    chore(prisma): instantiate prisma for next js

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client"
+import { env } from "@projectforum/env"
 
-const prisma = new PrismaClient()
+const prismaClientSingleton = () => {
+  return new PrismaClient({ log: ["error", "info", "warn"] })
+}
 
-export { prisma }
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+if (env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
